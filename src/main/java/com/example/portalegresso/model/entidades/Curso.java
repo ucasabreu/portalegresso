d5b90be0bee2b1e8391d494ac030fbe5481462b1
ucasabreu <ucasabreu@gmail.com>
2024-12-01 12:35:33 -0300

    add lombok buider

--- src/main/java/com/example/portalegresso/model/entidades/Curso.java
+++ src/main/java/com/example/portalegresso/model/entidades/Curso.java
@@ -7,12 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="curso")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 
